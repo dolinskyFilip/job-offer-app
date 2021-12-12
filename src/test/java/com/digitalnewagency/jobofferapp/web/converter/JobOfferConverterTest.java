fcecc6fcd2f2cfdbb6e9a7685c904400decadb21
filip <filipdolinski@gmail.com>
2021-12-12 22:09:37 +0100

    test added

--- src/test/java/com/digitalnewagency/jobofferapp/web/converter/JobOfferConverterTest.java
+++ src/test/java/com/digitalnewagency/jobofferapp/web/converter/JobOfferConverterTest.java
@@ -1,8 +1,38 @@
 package com.digitalnewagency.jobofferapp.web.converter;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.digitalnewagency.jobofferapp.model.entity.Category;
+import com.digitalnewagency.jobofferapp.model.entity.JobOffer;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 class JobOfferConverterTest {
 
+    private JobOfferConverter jobOfferConverter=new JobOfferConverter();
+
+    @Test
+    @DisplayName("should convert job offer list do dto list correct")
+    void shouldReturnDTOListCorrect() {
+
+        JobOffer jobOffer = new JobOffer();
+        jobOffer.setUserName("Test");
+        jobOffer.setCategory(Category.FOOD_DRINKS);
+        jobOffer.setStartDate(LocalDateTime.of(2021,12,1,12,00));
+        List<JobOffer> jobOffers= List.of(jobOffer);
+
+        var result= jobOfferConverter.convertListtoListDTO(jobOffers);
+
+        Assertions.assertThat(result)
+                .isNotNull()
+                .hasSize(1);
+        Assertions.assertThat(result.get(0).getUserName()).isEqualTo("Test");
+
+    }
 
 }
